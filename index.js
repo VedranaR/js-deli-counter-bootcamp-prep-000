2373bd1687db379e4bddfc61e55f45e527f2e1cd
Vedrana <vedrana_ravlic16@yahoo.com>
2018-01-28 12:04:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ function currentLine (katzDeliLine){
   if (katzDeliLine.length===0){
     return ("The line is currently empty.");
   } else {
-    return (`The line is currently: )
+    var newArr =[];
+    for(let i=1; i<katzDeliLine.length; i++){
+      newArr.push(i + "." + katzDeliLine[i]);
+    }
+    return (`The line is currently: ${newArr}`);
   }
 }
